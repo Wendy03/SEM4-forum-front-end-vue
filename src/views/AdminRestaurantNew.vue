80d746477fb93a4ce2d45a0147e4f2a0d50d3229
wendy03 <wendysu03@gmail.com>
2020-02-28 02:26:03 +0800

    Add Admin Restaurant New page

---
+++ src/views/AdminRestaurantNew.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container py-5">
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from "../components/AdminRestaurantForm";
+
+export default {
+  name: "AdminRestaurantNew",
+  components: {
+    AdminRestaurantForm
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ": " + value);
+      }
+    }
+  }
+};
+</script>
