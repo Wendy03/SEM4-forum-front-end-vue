d91f35a7a276ac29c66ef63b2c0d4181d74e2d11
wendy03 <wendysu03@gmail.com>
2020-02-29 00:12:25 +0800

    API Restaurants page

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -28,229 +28,10 @@ import NavTabs from "../components/NavTabs";
 import RestaurantCard from "../components/RestaurantCard";
 import RestaurantsNavPills from "../components/RestaurantsNavPills";
 import RestaurantsPagination from "../components/RestaurantsPagination";
+// STEP 1：透過 import 匯入剛剛撰寫好用來呼叫 API 的方法
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
 
-const dummyData = {
-  restaurants: [
-    {
-      id: 143,
-      name: "testrestaurant",
-      tel: "",
-      address: "",
-      opening_hours: "",
-      description: "",
-      image: null,
-      viewCounts: null,
-      createdAt: "2020-01-19T03:04:21.188Z",
-      updatedAt: "2020-01-19T03:04:21.188Z",
-      CategoryId: 109,
-      Category: {
-        id: 109,
-        name: "cool",
-        createdAt: "2019-12-17T11:36:08.937Z",
-        updatedAt: "2019-12-17T11:36:08.937Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 51,
-      name: "發大財0121牛排",
-      tel: "12345678",
-      address: "高雄市",
-      opening_hours: "00:00",
-      description: "123",
-      image: "https://i.imgur.com/CaeM9rg.jpg",
-      viewCounts: 27,
-      createdAt: "2019-11-22T06:50:58.472Z",
-      updatedAt: "2020-02-11T03:49:29.072Z",
-      CategoryId: 6,
-      Category: {
-        id: 6,
-        name: "美式料理",
-        createdAt: "2019-11-20T06:25:42.917Z",
-        updatedAt: "2019-11-20T06:25:42.917Z"
-      },
-      isFavorited: false,
-      isLiked: true
-    },
-    {
-      id: 144,
-      name: "麥當當",
-      tel: "123",
-      address: "麥當勞總部",
-      opening_hours: "03:33",
-      description: "",
-      image: null,
-      viewCounts: null,
-      createdAt: "2020-02-16T05:09:24.297Z",
-      updatedAt: "2020-02-16T05:09:24.297Z",
-      CategoryId: 6,
-      Category: {
-        id: 6,
-        name: "美式料理",
-        createdAt: "2019-11-20T06:25:42.917Z",
-        updatedAt: "2019-11-20T06:25:42.917Z"
-      },
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 14,
-      name: "Rahul Kilback",
-      tel: "1-573-027-3190",
-      address: "13538 Lula Roads",
-      opening_hours: "08:00",
-      description: "Ut ea repellat cumque illo sint. Possimus a magni ",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=66.19683181847653",
-      viewCounts: 77,
-      createdAt: "2019-11-20T06:25:42.925Z",
-      updatedAt: "2020-02-16T05:36:07.120Z",
-      CategoryId: 7,
-      Category: {
-        id: 7,
-        name: "複合式料理",
-        createdAt: "2019-11-20T06:25:42.917Z",
-        updatedAt: "2019-11-20T06:25:42.917Z"
-      },
-      isFavorited: true,
-      isLiked: true
-    },
-    {
-      id: 30,
-      name: "Vince Kassulke",
-      tel: "(302) 097-9821 x641",
-      address: "82815 Vivianne Inlet",
-      opening_hours: "08:00",
-      description: "nihil",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=60.72153192006735",
-      viewCounts: 112,
-      createdAt: "2019-11-20T06:25:42.928Z",
-      updatedAt: "2020-02-16T05:35:03.543Z",
-      CategoryId: null,
-      Category: null,
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 29,
-      name: "Mrs. Deonte Schmeler",
-      tel: "694-889-1934",
-      address: "4833 Ila Causeway",
-      opening_hours: "08:00",
-      description: "Ut officia pariatur quam perferendis quia nihil ex",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=33.27090568354101",
-      viewCounts: 9,
-      createdAt: "2019-11-20T06:25:42.928Z",
-      updatedAt: "2020-02-16T05:35:11.396Z",
-      CategoryId: null,
-      Category: null,
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 24,
-      name: "Tracy Kling",
-      tel: "602.671.9831",
-      address: "4776 Hazle Freeway",
-      opening_hours: "08:00",
-      description: "Libero fugit omnis inventore quasi maxime magnam a",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=28.34397872340968",
-      viewCounts: 6,
-      createdAt: "2019-11-20T06:25:42.927Z",
-      updatedAt: "2020-02-16T05:34:58.076Z",
-      CategoryId: null,
-      Category: null,
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 26,
-      name: "Dayna Roberts",
-      tel: "759-389-2695",
-      address: "9850 Kaia Circle",
-      opening_hours: "08:00",
-      description: "ipsum",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=3.7178880115385837",
-      viewCounts: null,
-      createdAt: "2019-11-20T06:25:42.928Z",
-      updatedAt: "2019-11-20T06:25:42.928Z",
-      CategoryId: null,
-      Category: null,
-      isFavorited: true,
-      isLiked: false
-    },
-    {
-      id: 34,
-      name: "Frida Jones",
-      tel: "(605) 542-9140 x08706",
-      address: "5248 Dare Forge",
-      opening_hours: "08:00",
-      description: "quidem quis quia",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=48.33317711631233",
-      viewCounts: null,
-      createdAt: "2019-11-20T06:25:42.929Z",
-      updatedAt: "2019-11-20T06:25:42.929Z",
-      CategoryId: null,
-      Category: null,
-      isFavorited: false,
-      isLiked: false
-    },
-    {
-      id: 36,
-      name: "Kale Nicolas",
-      tel: "1-129-966-2808",
-      address: "54065 Austin Plains",
-      opening_hours: "08:00",
-      description: "Est et id saepe tenetur et veritatis et. Qui simil",
-      image:
-        "https://loremflickr.com/320/240/restaurant,food/?random=11.056442083543061",
-      viewCounts: null,
-      createdAt: "2019-11-20T06:25:42.929Z",
-      updatedAt: "2019-11-20T06:25:42.929Z",
-      CategoryId: null,
-      Category: null,
-      isFavorited: false,
-      isLiked: false
-    }
-  ],
-  categories: [
-    {
-      id: 6,
-      name: "美式料理",
-      createdAt: "2019-11-20T06:25:42.917Z",
-      updatedAt: "2019-11-20T06:25:42.917Z"
-    },
-    {
-      id: 7,
-      name: "複合式料理",
-      createdAt: "2019-11-20T06:25:42.917Z",
-      updatedAt: "2019-11-20T06:25:42.917Z"
-    },
-    {
-      id: 109,
-      name: "cool",
-      createdAt: "2019-12-17T11:36:08.937Z",
-      updatedAt: "2019-12-17T11:36:08.937Z"
-    },
-    {
-      id: 110,
-      name: "ccc",
-      createdAt: "2020-01-12T16:58:30.105Z",
-      updatedAt: "2020-01-20T12:30:43.772Z"
-    }
-  ],
-  page: 1,
-  totalPage: [1, 2, 3, 4, 5],
-  prev: 1,
-  next: 2,
-  categoryId: ""
-};
 export default {
   components: {
     NavTabs,
@@ -268,15 +49,49 @@ export default {
     };
   },
   created() {
-    this.fetchRestaurants();
+    // STEP 2：在 created 的時候呼叫 fetchRestaurants 方法
+    // 這裡會向伺服器請求第一頁且不分餐廳類別的資料
+    const { page = 1, categoryId = "" } = this.$route.query;
+    this.fetchRestaurants({
+      page,
+      categoryId
+    });
+  },
+  // 使用 beforeRouteUpdate 方法取得使用者路由變化
+  beforeRouteUpdate(to, from, next) {
+    const { page = 1, categoryId = "" } = to.query;
+    this.fetchRestaurants({ page, categoryId });
+    next();
   },
   methods: {
-    fetchRestaurants() {
-      this.categories = dummyData.categories;
-      this.categoryId = dummyData.categoryId;
-      this.currentPage = dummyData.page;
-      this.restaurants = dummyData.restaurants;
-      this.totalPage = dummyData.totalPage.length;
+    // STEP 1：將 fetchRestaurants 改成 async...await 的語法
+    // 並且可以帶入參數 page 與 categoryId
+    // 呼叫 API 後取得 response
+    async fetchRestaurants({ page, categoryId }) {
+      try {
+        const response = await restaurantsAPI.getRestaurants({
+          page,
+          categoryId
+        });
+        // STEP 3：將 response 中的 data 和 statusText 取出
+        const { data, statusText } = response;
+        // STEP 4s：如果 statusText 不是 OK 的話則進入錯誤處理
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        // STEP 4：將從伺服器取得的 data 帶入 Vue 內
+        this.categories = data.categories;
+        this.categoryId = data.categoryId;
+        this.currentPage = data.page;
+        this.restaurants = data.restaurants;
+        this.totalPage = data.totalPage.length;
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取得餐廳資料，請稍後再試",
+          icon: "error"
+        });
+      }
     }
   }
 };
