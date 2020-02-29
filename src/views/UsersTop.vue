b2ee4c4874ded1f470d697ca7e57eaa780766d56
wendy03 <wendysu03@gmail.com>
2020-02-29 10:11:36 +0800

    API Top Users

--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -31,133 +31,16 @@
 
 <script>
 import NavTabs from "../components/NavTabs";
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
+import { emptyImageFilter } from "./../utils/mixins";
 
-const dummyData = {
-  users: [
-    {
-      id: 1,
-      name: "root",
-      email: "root@example.com",
-      password: "$2a$10$alLLwv1Kn0tC9euHs6Llwen8uif7jQfU9DPaybXRuGn83ZfKzW56G",
-      isAdmin: true,
-      image: "https://i.imgur.com/JtQJRMZ.png",
-      createdAt: "2019-11-20T06:25:42.685Z",
-      updatedAt: "2019-11-21T09:55:30.970Z",
-      Followers: [
-        {
-          id: 1,
-          name: "root1",
-          email: "root@example.com",
-          password:
-            "$2a$10$alLLwv1Kn0tC9euHs6Llwen8uif7jQfU9DPaybXRuGn83ZfKzW56G",
-          isAdmin: true,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.456Z",
-          updatedAt: "2019-11-25T06:53:28.542Z",
-          Followship: {
-            followerId: 1,
-            followingId: 2,
-            createdAt: "2020-01-21T12:52:03.002Z",
-            updatedAt: "2020-01-21T12:52:03.002Z"
-          }
-        },
-        {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$ESv6iQjQ8oEe3/XGjw00PuSh1kjmG6Dkhd4YXa50boTlncJDxljAy",
-          isAdmin: false,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.685Z",
-          updatedAt: "2019-11-21T09:55:30.970Z",
-          Followship: {
-            followerId: 2,
-            followingId: 2,
-            createdAt: "2019-12-08T15:48:13.364Z",
-            updatedAt: "2019-12-08T15:48:13.364Z"
-          }
-        }
-      ],
-      FollowerCount: 2,
-      isFollowed: true
-    },
-    {
-      id: 2,
-      name: "user1",
-      email: "user1@example.com",
-      password: "$2a$10$ESv6iQjQ8oEe3/XGjw00PuSh1kjmG6Dkhd4YXa50boTlncJDxljAy",
-      isAdmin: false,
-      image: "https://i.imgur.com/XooCt5K.png",
-      createdAt: "2019-11-20T06:25:42.685Z",
-      updatedAt: "2019-11-21T09:55:30.970Z",
-      Followers: [
-        {
-          id: 1,
-          name: "root1",
-          email: "root@example.com",
-          password:
-            "$2a$10$alLLwv1Kn0tC9euHs6Llwen8uif7jQfU9DPaybXRuGn83ZfKzW56G",
-          isAdmin: true,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.456Z",
-          updatedAt: "2019-11-25T06:53:28.542Z",
-          Followship: {
-            followerId: 1,
-            followingId: 2,
-            createdAt: "2020-01-21T12:52:03.002Z",
-            updatedAt: "2020-01-21T12:52:03.002Z"
-          }
-        },
-        {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$ESv6iQjQ8oEe3/XGjw00PuSh1kjmG6Dkhd4YXa50boTlncJDxljAy",
-          isAdmin: false,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.685Z",
-          updatedAt: "2019-11-21T09:55:30.970Z",
-          Followship: {
-            followerId: 2,
-            followingId: 2,
-            createdAt: "2019-12-08T15:48:13.364Z",
-            updatedAt: "2019-12-08T15:48:13.364Z"
-          }
-        }
-      ],
-      FollowerCount: 1,
-      isFollowed: true
-    },
-    {
-      id: 3,
-      name: "user2",
-      email: "user2@example.com",
-      password: "$2a$10$0Izt5W6Q4iphLq1ORgsKQu9jbEFdRD3.UaX/HQQg0M4Hp8yWV.XuS",
-      image: "https://i.imgur.com/1T9ddHb.png",
-      isAdmin: false,
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-23T01:16:52.000Z",
-      Followers: [],
-      FollowerCount: 0,
-      isFollowed: false
-    }
-  ]
-};
 export default {
   name: "RestaurantsFeeds",
   components: {
     NavTabs
   },
-  filters: {
-    emptyImage(src) {
-      if (!src) {
-        return "http://via.placeholder.com/300x300?text=No+Image";
-      }
-      return src;
-    }
-  },
+  mixins: [emptyImageFilter],
   data() {
     return {
       users: []
@@ -167,30 +50,84 @@ export default {
     this.fetchTopUsers();
   },
   methods: {
-    fetchTopUsers() {
-      this.users = dummyData.users;
-    },
-    addFollowing(userId) {
-      this.users = this.users.map(user => {
-        if (user.id !== userId) {
-          return user;
+    async fetchTopUsers() {
+      try {
+        const { data, statusText } = await usersAPI.getTopUsers();
+
+        if (statusText !== "OK") {
+          throw new Error(statusText);
         }
-        return {
-          ...user,
-          isFollowed: true
-        };
-      });
+
+        this.users = data.users;
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取得美食達人，請稍後再試",
+          icon: "error"
+        });
+      }
     },
-    removeFollowing(userId) {
-      this.users = this.users.map(user => {
-        if (user.id !== userId) {
-          return user;
+    async addFollowing(userId) {
+      try {
+        const { data, statusText } = await usersAPI.addFollowing({
+          userId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
         }
-        return {
-          ...user,
-          isFollowed: false
-        };
-      });
+
+        this.users = this.users
+          .map(user => {
+            if (user.id !== userId) {
+              return user;
+            }
+
+            return {
+              ...user,
+              FollowerCount: user.FollowerCount + 1,
+              isFollowed: true
+            };
+          })
+          .sort((a, b) => b.FollowerCount - a.FollowerCount);
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法加入追蹤，請稍後再試",
+          icon: "error"
+        });
+      }
+    },
+    async removeFollowing(userId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFollowing({
+          userId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.users = this.users
+          .map(user => {
+            if (user.id !== userId) {
+              return user;
+            }
+
+            return {
+              ...user,
+              FollowerCount: user.FollowerCount - 1,
+              isFollowed: false
+            };
+          })
+          .sort((a, b) => b.FollowerCount - a.FollowerCount);
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取消追蹤，請稍後再試",
+          icon: "error"
+        });
+      }
     }
   }
 };
