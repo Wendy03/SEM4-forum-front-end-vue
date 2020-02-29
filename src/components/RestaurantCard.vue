207a1f8c3319ececdc4f4305a16ecf4c14145a8b
wendy03 <wendysu03@gmail.com>
2020-02-29 09:52:54 +0800

    API RestaurantCard

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -3,7 +3,7 @@
     <div class="card mb-4">
       <img
         class="card-img-top"
-        :src="restaurant.image"
+        :src="restaurant.image | emptyImage"
         alt="Card image cap"
         width="286px"
         height="180px"
@@ -14,7 +14,7 @@
             :to="{ name: 'restaurant', params: { id: restaurant.id }}"
           >{{ restaurant.name }}</router-link>
         </p>
-        <span class="badge badge-secondary">{{ restaurant.Category.name }}</span>
+        <span class="badge badge-secondary">{{ restaurant.Category | formatCategoryName }}</span>
         <p class="card-text text-truncate">{{ restaurant.description }}</p>
       </div>
       <div class="card-footer">
@@ -22,25 +22,25 @@
           v-if="restaurant.isFavorited"
           type="button"
           class="btn btn-danger btn-border favorite mr-2"
-          @click.stop.prevent="deleteFavorite"
+          @click.stop.prevent="deleteFavorite(restaurant.id)"
         >移除最愛</button>
         <button
           v-else
           type="button"
           class="btn btn-primary btn-border favorite mr-2"
-          @click.stop.prevent="addFavorite"
+          @click.stop.prevent="addFavorite(restaurant.id)"
         >加到最愛</button>
         <button
           v-if="restaurant.isLiked"
           type="button"
           class="btn btn-danger like mr-2"
-          @click.stop.prevent="deleteLike"
+          @click.stop.prevent="deleteLike(restaurant.id)"
         >Unlike</button>
         <button
           v-else
           type="button"
           class="btn btn-primary like mr-2"
-          @click.stop.prevent="addLike"
+          @click.stop.prevent="addLike(restaurant.id)"
         >Like</button>
       </div>
     </div>
@@ -48,7 +48,12 @@
 </template>
 
 <script>
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
+import { emptyImageFilter, categoryFilter } from "./../utils/mixins";
+
 export default {
+  mixins: [emptyImageFilter, categoryFilter],
   props: {
     initialRestaurant: {
       type: Object,
@@ -61,29 +66,102 @@ export default {
     };
   },
   methods: {
-    addFavorite() {
-      this.restaurant = {
-        ...this.restaurant, // 保留餐廳原有資料
-        isFavorited: true
-      };
+    // STEP 2: 將 addFavorite 改成 async...await 語法
+    async addFavorite(restaurantId) {
+      try {
+        // STEP 3: 使用撰寫好的 addFavorite 方法去呼叫 API，並取得回傳內容
+        const { data, statusText } = await usersAPI.addFavorite({
+          restaurantId
+        });
+
+        // STEP 4: 若請求過程有錯，則進到錯誤處理
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        // STEP 5: 請求成功的話，改變 Vue 內的資料狀態
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: true
+        };
+      } catch (error) {
+        // STEP 6: 請求失敗的話則跳出錯誤提示
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳加入最愛，請稍後再試",
+          icon: "error"
+        });
+      }
     },
-    deleteFavorite() {
-      this.restaurant = {
-        ...this.restaurant, // 保留餐廳原有資料
-        isFavorited: false
-      };
+    async deleteFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFavorite({
+          restaurantId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳從最愛移除，請稍後再試",
+          icon: "error"
+        });
+      }
     },
-    addLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: true
-      };
+    async addLike(restaurantId) {
+      try {
+        // STEP 3: 使用撰寫好的 addFavorite 方法去呼叫 API，並取得回傳內容
+        const { data, statusText } = await usersAPI.addFavorite({
+          restaurantId
+        });
+
+        // STEP 4: 若請求過程有錯，則進到錯誤處理
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        // STEP 5: 請求成功的話，改變 Vue 內的資料狀態
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: true
+        };
+      } catch (error) {
+        // STEP 6: 請求失敗的話則跳出錯誤提示
+        Toast.fire({
+          type: "error",
+          title: "無法按讚，請稍後再試",
+          icon: "error"
+        });
+      }
     },
-    deleteLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: false
-      };
+    async deleteLike(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFavorite({
+          restaurantId
+        });
+
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取消按讚，請稍後再試",
+          icon: "error"
+        });
+      }
     }
   }
 };
