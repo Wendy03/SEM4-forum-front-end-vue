8c218202b8a1019cb36e333233597c5bec96a481
wendy03 <wendysu03@gmail.com>
2020-03-03 00:06:53 +0800

    API Admin User page

--- src/views/AdminUsers.vue
+++ src/views/AdminUsers.vue
@@ -32,132 +32,10 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
 import AdminNav from "@/components/AdminNav";
-
-const dummyData = {
-  users: [
-    {
-      id: 1,
-      name: "root",
-      email: "root@example.com",
-      password: "$2a$10$alLLwv1Kn0tC9euHs6Llwen8uif7jQfU9DPaybXRuGn83ZfKzW56G",
-      isAdmin: true,
-      image: "https://i.imgur.com/JtQJRMZ.png",
-      createdAt: "2019-11-20T06:25:42.685Z",
-      updatedAt: "2019-11-21T09:55:30.970Z",
-      Followers: [
-        {
-          id: 1,
-          name: "root1",
-          email: "root@example.com",
-          password:
-            "$2a$10$alLLwv1Kn0tC9euHs6Llwen8uif7jQfU9DPaybXRuGn83ZfKzW56G",
-          isAdmin: true,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.456Z",
-          updatedAt: "2019-11-25T06:53:28.542Z",
-          Followship: {
-            followerId: 1,
-            followingId: 2,
-            createdAt: "2020-01-21T12:52:03.002Z",
-            updatedAt: "2020-01-21T12:52:03.002Z"
-          }
-        },
-        {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$ESv6iQjQ8oEe3/XGjw00PuSh1kjmG6Dkhd4YXa50boTlncJDxljAy",
-          isAdmin: false,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.685Z",
-          updatedAt: "2019-11-21T09:55:30.970Z",
-          Followship: {
-            followerId: 2,
-            followingId: 2,
-            createdAt: "2019-12-08T15:48:13.364Z",
-            updatedAt: "2019-12-08T15:48:13.364Z"
-          }
-        }
-      ],
-      FollowerCount: 2,
-      isFollowed: true
-    },
-    {
-      id: 2,
-      name: "user1",
-      email: "user1@example.com",
-      password: "$2a$10$ESv6iQjQ8oEe3/XGjw00PuSh1kjmG6Dkhd4YXa50boTlncJDxljAy",
-      isAdmin: false,
-      image: "https://i.imgur.com/XooCt5K.png",
-      createdAt: "2019-11-20T06:25:42.685Z",
-      updatedAt: "2019-11-21T09:55:30.970Z",
-      Followers: [
-        {
-          id: 1,
-          name: "root1",
-          email: "root@example.com",
-          password:
-            "$2a$10$alLLwv1Kn0tC9euHs6Llwen8uif7jQfU9DPaybXRuGn83ZfKzW56G",
-          isAdmin: true,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.456Z",
-          updatedAt: "2019-11-25T06:53:28.542Z",
-          Followship: {
-            followerId: 1,
-            followingId: 2,
-            createdAt: "2020-01-21T12:52:03.002Z",
-            updatedAt: "2020-01-21T12:52:03.002Z"
-          }
-        },
-        {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$ESv6iQjQ8oEe3/XGjw00PuSh1kjmG6Dkhd4YXa50boTlncJDxljAy",
-          isAdmin: false,
-          image: null,
-          createdAt: "2019-11-20T06:25:42.685Z",
-          updatedAt: "2019-11-21T09:55:30.970Z",
-          Followship: {
-            followerId: 2,
-            followingId: 2,
-            createdAt: "2019-12-08T15:48:13.364Z",
-            updatedAt: "2019-12-08T15:48:13.364Z"
-          }
-        }
-      ],
-      FollowerCount: 1,
-      isFollowed: true
-    },
-    {
-      id: 3,
-      name: "user2",
-      email: "user2@example.com",
-      password: "$2a$10$0Izt5W6Q4iphLq1ORgsKQu9jbEFdRD3.UaX/HQQg0M4Hp8yWV.XuS",
-      image: "https://i.imgur.com/1T9ddHb.png",
-      isAdmin: false,
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-23T01:16:52.000Z",
-      Followers: [],
-      FollowerCount: 0,
-      isFollowed: false
-    }
-  ]
-};
-
-const dummyUser = {
-  currentUser: {
-    id: 1,
-    name: "管理者",
-    email: "root@example.com",
-    image: "https://i.pravatar.cc/300",
-    isAdmin: true
-  },
-  isAuthenticated: true
-};
 
 export default {
   components: {
@@ -165,27 +43,56 @@ export default {
   },
   data() {
     return {
-      users: [],
-      currentUser: dummyUser.currentUser
+      users: []
     };
+  },
+  computed: {
+    ...mapState(["currentUser"])
   },
   created() {
     this.fetchUser();
   },
+
   methods: {
-    fetchUser() {
-      this.users = dummyData.users;
-    },
-    toggleUserRole({ userId, isAdmin }) {
-      this.users = this.users.map(user => {
-        if (user.id !== userId) {
-          return user;
+    async fetchUser() {
+      try {
+        const { data, statusText } = await adminAPI.users.get();
+        if (statusText !== "OK") {
+          throw new Error(statusText);
         }
-        return {
-          ...user,
-          isAdmin: !isAdmin
-        };
-      });
+        this.users = data.users;
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取得使用者資料，請稍後再試"
+        });
+      }
+    },
+    async toggleUserRole({ userId, isAdmin }) {
+      try {
+        const changeToAdmin = !isAdmin;
+        const { data, statusText } = await adminAPI.users.update({
+          userId,
+          isAdmin: changeToAdmin
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.users = this.users.map(user => {
+          if (user.id !== userId) {
+            return user;
+          }
+          return {
+            ...user,
+            isAdmin: changeToAdmin
+          };
+        });
+      } catch (error) {
+        Toast.fire({
+          icon: "error",
+          title: "無法變更使用者權限，請稍後再試"
+        });
+      }
     }
   }
 };
